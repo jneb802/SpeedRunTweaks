1590ad5d91da7309495f7a8464456aef240fb76e
warp <73610029+jneb802@users.noreply.github.com>
2025-04-30 14:27:24 -0700

    don't display world seed in menu

--- SpeedRunTweaks/Src/GUI_Patch.cs
+++ SpeedRunTweaks/Src/GUI_Patch.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Collections.Generic;
 using HarmonyLib;
+using TMPro;
+using UnityEngine;
 
 namespace SpeedRunTweaks;
 
@@ -10,7 +13,28 @@ public class GUI_Patch
     {
         public static void Postfix(FejdStartup __instance)
         {
-            __instance.m_newWorldSeed.text = "XXXXXX";
+            GameObject panel = __instance.m_createWorldPanel.transform.GetChild(0).gameObject;
+            GameObject seedField = panel.transform.GetChild(3).gameObject;
+            GameObject textArea = seedField.transform.GetChild(0).gameObject;
+            GameObject text = textArea.transform.GetChild(2).gameObject;
+            TextMeshProUGUI textMeshProUGUI = text.GetComponent<TextMeshProUGUI>();
+            textMeshProUGUI.fontSize = 0;
+        }
+    }
+    
+    [HarmonyPatch(typeof(FejdStartup), nameof(FejdStartup.UpdateWorldList))]
+    public static class FejdStartup_UpdateWorldList_Patch
+    {
+        public static void Postfix(FejdStartup __instance)
+        {
+            RectTransform worldListRoot = __instance.m_worldListRoot;
+            
+            for (int i = 0; i < worldListRoot.childCount; i++)
+            {
+                GameObject worldElement = worldListRoot.GetChild(i).gameObject;
+                GameObject seed = worldElement.transform.GetChild(4).gameObject;
+                seed.SetActive(false);
+            }
         }
     }
 }
--- SpeedRunTweaks/Src/SpeedRunTweaks.cs
+++ SpeedRunTweaks/Src/SpeedRunTweaks.cs
@@ -15,7 +15,7 @@ namespace SpeedRunTweaks
     public class SpeedRunTweaksPlugin : BaseUnityPlugin
     {
         internal const string ModName = "SpeedrunTweaks";
-        internal const string ModVersion = "1.0.0";
+        internal const string ModVersion = "1.0.2";
         internal const string Author = "warpalicious";
         private const string ModGUID = Author + "." + ModName;
         private static string ConfigFileName = ModGUID + ".cfg";
